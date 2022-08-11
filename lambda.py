75ec57d0494569444908d1f42456192f8feba6aa
Orangemaster96 <107160973+Orangemaster96@users.noreply.github.com>
2022-08-10 19:29:38 -0500

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,8 @@
+#sqaure with lambda
+my_list = [5,4,3]
+print(list(map(lambda item: item**2, my_list)))
+
+#list sorting based on second number
+a = [(0,2), (4,3), (9,9), (10, -1)]
+a.sort(key=lambda x: x[1])
+print(a)
