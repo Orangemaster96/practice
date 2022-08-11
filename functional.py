804fcde2730913dc1003c18f2fad6b7b3b8af186
Orangemaster96 <107160973+Orangemaster96@users.noreply.github.com>
2022-08-10 19:08:04 -0500

    Add files via upload

---
+++ functional.py
@@ -0,0 +1,41 @@
+from functools import reduce
+
+#1 Capitalize all of the pet names and print the list
+my_pets = ['sisi', 'bibi', 'titi', 'carla']
+new_list=[]
+for item in my_pets:
+    first_letter=item[0].upper()
+    word= first_letter + item[1:]
+    new_list.append(word)
+print(new_list)
+def capitalize(item):
+    first_letter=item[0].upper()
+    word= first_letter + item[1:]
+    return word
+print(list(map(capitalize, my_pets)))
+
+#2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
+my_strings = ['a', 'b', 'c', 'd', 'e']
+my_numbers = [5,4,3,2,1]
+my_numbers.sort()
+tuple = list(zip(my_strings, my_numbers))
+print(tuple)
+
+
+#3 Filter the scores that pass over 50%
+scores = [73, 20, 65, 19, 76, 100, 88]
+def overfifty(item):
+    if int(item) > 50:
+        return True
+    else:
+        return False
+
+scores = list(filter(overfifty, scores))
+print(scores)
+
+
+
+#4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+def accumulator(acc,item):
+    return acc + item
+print(reduce(accumulator,(my_numbers + scores)))
